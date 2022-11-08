6d644088ccf6a4f7ba93da60d59564ec08f4bb3e
shitasd <slgkf654@naver.com>
2022-11-08 14:28:56 +0900

    221108변경사항1

--- opencv_download/opencv_download.py
+++ opencv_download/opencv_download.py
@@ -2,23 +2,49 @@
 #PyPI 다운
 #파이선 버전을 3.7.9이하로 다운(OPENCV가 3.7.9위에서는 미지원)
 
+# python 은 파이선 홈페이지에서 다운로드
+
 # cmd를 관리자 권한으로 시행
 
-# curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+# unable to create process using에러 시
+# python -m pip install XXX
 
-# python get-pip.py
+# pip 안될시
+# pyhon get-pip.py 
+# 강제실행명령어
+
+# 카메라 연결
+# 웹캠 바로 연결하거나 
+# 휴대폰, pc에 droid cam설치
+# 휴대폰 usb로 연결시 휴대폰정보 > 소프트웨어 정보 > 빌드번호 5번 터치 > 패턴입력
+# 휴대폰정보 밑 개발자 정보 생성됨
+# usb 디버깅 허용 > 드로이드캠(pc) > connect usb > 연결
+
+#터미널에서 pip install opencv-python
+#-업데이트시 python -m pip install --upgrade pip
+
+#pip 안될대 
+# pip install bs4
+# pip install beautifulsoup4
+# pip install django
+
+
+
+########################################
+
+# cmd 순서
 
 # pip install --upgrade pip
 
-# pip install notebook
+# pip install opencv-python
 
 # pip intall numpy
 
+# pip install notebook
+
 # pip install matplotlib
 
-# python idle 실행
-# python 은 파이선 홈페이지에서 다운로드
-# 카메라 연결
+################################# 아래는 예제를 실행하는 방법
 
 #새 터미널 창
 
@@ -26,7 +52,7 @@
 
 # f1을 누른 후 찾기
 
-# bin => python
+# script => python
 
 # ctrl + ~
 
@@ -38,19 +64,9 @@
 
 # 휴지통 누른 후 ctrl + ~
 
-#터미널에서 pip install opencv-python
-#-업데이트시 python -m pip install --upgrade pip
-
 #python 인터프린터를 작업영역을 선택하지 말것
-
-#pip 안될대 
-# pip install bs4
-# pip install beautifulsoup4
-# pip install django
-
-
-
-
+#인터프린터 설정시 파일탐색기 들어가서 숨김파일 표시
+#위치 : c > 사용자 > admin(또는 자기이름) > appdata > local > programs > python > python37
 
 
 #
