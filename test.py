307f563fc27715df66ad7af7a1c283c31cbbb2be
minyeob <aksel20010423@gmail.com>
2022-11-27 23:21:55 +0900

    221127변경사항

---
+++ test.py
@@ -0,0 +1,22 @@
+# 우노로 실행
+# https://boa9448.tistory.com/8
+
+import time
+
+import serial
+
+arduino = serial.Serial(port = "COM10", baudrate = 9600)
+
+time.sleep(3)
+
+arduino.write(b"test")
+
+time.sleep(1)
+
+data = arduino.read_all()
+
+print(data)
+
+arduino.close()
+
+# 테스트 결과 b'test'
